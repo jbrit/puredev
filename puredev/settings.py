2dec38bb56140b6eae683c5c5e0ac689795566bb
jbrit <jb4realfaith@gmail.com>
2019-09-05 05:06:00 +0100

    My Initial commit

--- puredev/settings.py
+++ puredev/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 STATIC_URL = '/assets/'
 STATIC_ROOT = 'main-static'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    #os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR,"base_assets"),
 ]
